04b62d6fb913e4583e1bbf402040dd651f7f077c
CoDeRgAnEsh <ganeshkumartk@outlook.com>
2019-02-11 11:16:20 +0000

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int asciiToBinary(int input) {
+        int result = 0, i = 1, remainder;
+
+        /* convert decimal to binary format */
+        while (input > 0) {
+                remainder = input % 2;
+                result = result + (i * remainder);
+                input = input / 2;
+                i = i * 10;
+        }
+}
+
+int main(){
+  
+    char str[100];
+    int i=0;
+    printf("Enter any string: ");
+    scanf("%s",str);
+    len =strlen(str);
+    printf("ASCII values of each characters of given string: ");
+    while(str[i])
+         printf("%d ",str[i++]);
+    
+    printf("Binary values of each characters of given string: ");
+    while (len!=0) 
+        {
+                printf("\t %d \t",asciiToBinary(toascii(ch)));
+        }
+   
+    return 0;
+}
