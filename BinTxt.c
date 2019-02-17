7ae0e8fe9cc0ed794660e248510df27379e7f645
CoDeRgAnEsh <ganeshkumartk@outlook.com>
2019-02-17 16:32:00 +0530

    Update BinTxt.c

--- BinTxt.c
+++ BinTxt.c
@@ -1,33 +1,76 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+/* C source code for Text to binary conversion 
+    part of M2M Assignment */
 
-int main()
-{ 
-    char str[100];
-    int len;
-    printf(" Enter the string:");
-    scanf("%s",str);
-    printf("\n Binary is: %s", stringToBinary(str));
-    return 0;
-}    
-    
-    
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-char* stringToBinary(char* s) {
-    if(s == NULL) return 0; /* no input string */
-    size_t len = strlen(s);
-    char *binary = malloc(len*8 + 1); // each char is one byte (8 bits) and + 1 at the end for null terminator
-    binary[0] = '\0';
-    for(size_t i = 0; i < len; ++i) {
-        char ch = s[i];
-        for(int j = 7; j >= 0; --j){
-            if(ch & (1 << j)) {
-                strcat(binary,"1");
-            } else {
-                strcat(binary,"0");
-            }
-        }
+unsigned long binaryToDecimal(char *binary, int length);
+void textToBinary(char *text, int textLength, char *binary, int binaryLength);
+void decimalToBinary(int decimal, char *octet);
+
+int main(void)
+{
+	char text[101];
+    char *binary;
+	int textLength, binaryLength;
+
+    printf("Enter text: ");
+    scanf("%100[^\n]s", text);
+	textLength = strlen(text);
+    binaryLength = textLength * 9;      // 8 binary digits + 1 space separator
+    binary = malloc(binaryLength + 1);  // + 1 null terminator
+    if(binary == NULL)
+        exit(1);
+    
+    textToBinary(text, textLength, binary, binaryLength);
+    printf("Your binary encoding is:\n%s\n", binary);
+    
+    free(binary);
+	
+	return 0;
+}
+
+void textToBinary(char *text, int textLength, char *binary, int binaryLength)
+{
+    char *octet = malloc(9);
+    if(octet == NULL)
+        exit(1);
+    while(*text)
+    {
+        decimalToBinary(*text, octet);
+        while(*octet)
+            *binary++ = *octet++;
+        *binary++ = ' ';
+        ++text;
+        octet -= 8;
     }
-    return binary;
+    *binary = '\0';
+    binary -= binaryLength;
+    free(octet);
+}
+
+void decimalToBinary(int decimal, char *octet)
+{
+	octet += 8;
+	*octet = '\0';
+	if(decimal == 0)
+	{
+		octet -= 8;
+		octet = "00000000";
+	}
+	else
+	{
+		while(decimal > 0)
+		{
+			--octet;
+			*octet = decimal % 2 + '0';
+			decimal = decimal / 2;
+		}
+	}
+    while(strlen(octet) < 8)
+    {
+        --octet;
+        *octet = '0';
+    }
 }
